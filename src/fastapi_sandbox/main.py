7bab14840dacf40779d5c4a7bdee87a34c1b154f
coba-yu <yukob.formal@gmail.com>
2024-06-02 23:11:59 +0900

    feat: /chat/conversations

--- src/fastapi_sandbox/main.py
+++ src/fastapi_sandbox/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
 from .routers import accounts
-from .routers.chat import chat_messages
-from .routers.chat import files
+from .routers.chat import chat_messages, conversations, files
 
 app = FastAPI()
 app.include_router(accounts.router)
 app.include_router(chat_messages.router)
+app.include_router(conversations.router)
 app.include_router(files.router)
